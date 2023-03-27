dabda498662429245a084fc4c124a17102c3f0b8
AnthX0 <victorglezb@gmail.com>
2023-03-26 23:32:50 -0700

    Carga Inicial

---
+++ src/main/java/org/itson/main/AgenciaTransito.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.itson.main;
+
+/**
+ * @author Victor y Samuel
+ */
+public class AgenciaTransito {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
