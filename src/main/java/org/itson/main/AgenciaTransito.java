735b9ec3023a2693604672273a91856242577802
Samuel Vega <atrap.rap00z0@gmail.com>
2023-03-27 00:13:34 -0700

    Cambio de prueba

--- src/main/java/org/itson/main/AgenciaTransito.java
+++ src/main/java/org/itson/main/AgenciaTransito.java
@@ -10,6 +10,6 @@ package org.itson.main;
 public class AgenciaTransito {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hola Mundo!");
     }
 }
